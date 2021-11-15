1a3162b004146ad58e8a3f0aa613beb36d26d9d7
Julián Melero <hola@julianmelero.com>
2021-11-15 15:23:38 +0100

    healthcheck execution time

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from datetime import datetime
 app = Flask(__name__)
-
+initial_time = datetime.today()
 
 @app.route('/')
 def index():
@@ -10,5 +10,6 @@ def index():
 
 @app.route('/health')
 def health():    
-    return datetime.today().strftime('%d/%m/%Y %H:%M:%S')
+    execution_time = datetime.today() - initial_time
+    return "Today is " + datetime.today().strftime('%d/%m/%Y %H:%M:%S') + "\n Server execution time:" +  str(execution_time)
 
