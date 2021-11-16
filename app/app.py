b7f4a4fac03b5ca1df63aeff9050e26c844602e7
Julián Melero <hola@julianmelero.com>
2021-11-16 08:46:40 +0100

    Change/Add EndPoints

--- app/app.py
+++ app/app.py
@@ -1,20 +1,42 @@
-from flask import Flask, request
+from flask import Flask, request, Response
 from datetime import datetime
+
+from flask.json import jsonify
 app = Flask(__name__)
 initial_time = datetime.today()
 
 @app.route('/')
 def index():
-    return "Hi!"
+    return jsonify("Hi!")
 
 
 @app.route('/health')
-def health():    
+def health():
+       
     execution_time = datetime.today() - initial_time
-    return "Today is " + datetime.today().strftime('%d/%m/%Y %H:%M:%S') + "\n Server execution time:" +  str(execution_time)
+    times = {
+        "today":datetime.today().strftime('%d/%m/%Y %H:%M:%Ss'),
+        "execution_time":  str(execution_time)
+    }
+    return Response(jsonify(times), status=202)
+    
 
 @app.route('/sayhello', methods=['POST'])
 def sayhello():
     res =  request.get_json()
     if res['mensaje'] == 'hello':
         return "Hola mundo!"
+
+@app.route('/gatos')
+def gatos():
+    return ""
+
+
+@app.route('/perros')
+def perros():
+    return ""
+
+@app.route('/image/<imagen>')
+def image(imagen):
+    return ""
+
